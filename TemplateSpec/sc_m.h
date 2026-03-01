dfe07803dedb9faa764c71f46ed5f451fb61edd3
Lionel Desorme <lionel.desorme@free.fr>
2026-03-01 11:00:15 +0100

    Update 7

--- TemplateSpec/sc_m.h
+++ TemplateSpec/sc_m.h
@@ -2,7 +2,6 @@
 
 #include <iostream>
 
-
 enum class MyType
 {
     TypeA,
@@ -10,23 +9,29 @@ enum class MyType
     TypeError
 };
 
-std::string toString(MyType type)
-{
-    switch (type)
-    {
-        case MyType::TypeA: return "TypeA";
-        case MyType::TypeB: return "TypeB";
-        case MyType::TypeError: return "TypeError";
-        default: return "Unknown";
-    }
-}
-
+// Object => type convertor
 template <typename T>
 inline MyType getType()
 {
     static_assert(false, "Missing specialization");
     std::cout << "Error: No specialization for this type" << std::endl;
     return MyType::TypeError;
+}
+struct TotoA;
+template <> inline MyType getType<TotoA>() { return MyType::TypeA; }
+struct TotoB;
+template <> inline MyType getType<TotoB>() { return MyType::TypeB; }
+// Object => type convertor
+
+std::string toString(MyType type)
+{
+    switch (type)
+    {
+    case MyType::TypeA: return "TypeA";
+    case MyType::TypeB: return "TypeB";
+    case MyType::TypeError: return "TypeError";
+    default: return "Unknown";
+    }
 }
 
 struct Toto
@@ -61,5 +66,3 @@ struct TotoNoSpec : public TotoB
 };
 
 
-template <> inline MyType getType<TotoA>() { return MyType::TypeA; }
-template <> inline MyType getType<TotoB>() { return MyType::TypeB; }
